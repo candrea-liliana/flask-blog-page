6ca84d98d22adac00010126ab284b2c520e58a3e
Liliana Candrea <candrealiliana94@gmail.com>
2026-03-24 16:30:27 +0200

    Added templates structure. Navbar, main content, sidebar for updates. Buttons for register and login. Render dynamic dummy content. Added some basic .css styling.

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Lili Candrea',
+        'title': 'First blog post',
+        'content': 'First content post',
+        'date_posted': 'Martie 21, 2026'
+    },
+    {
+        'author': 'Anabelle',
+        'title': 'Second blog post',
+        'content': 'Second content post',
+        'date_posted': 'Martie 22, 2026'
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
+
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
-if  __name__ == '__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
