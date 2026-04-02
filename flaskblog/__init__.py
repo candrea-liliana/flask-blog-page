48603bc294704eeec528cc753996b4753abfdadc
Liliana Candrea <candrealiliana94@gmail.com>
2026-04-02 17:32:15 +0300

    Create an authentication system using flask-bcrypt and flask-login extensions. The user can log in and log out from application. Save the User data to DB. Create hashed passwords for secure storage. Make sure the User don't already exist in DB by verifying the username and email data - in RegistrationForms. Add logout and account routes. Create an 'Account' template. Check if the current user is authenticated and add restrictions. Change the right side navigation if it has a current user authenticated to show 'Login' and 'Account' options.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app) # Set a DB instance
+bcrypt = Bcrypt(app) # Set an encrypting instance
+login_manager = LoginManager(app) # Set a Login manager instance
+login_manager.login_view = 'login' # Set the login manager by the login view
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
