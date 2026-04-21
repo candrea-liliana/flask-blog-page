1a9e4340568bfc62ba3eb7c0c691a17819307e93
Liliana Candrea <candrealiliana94@gmail.com>
2026-04-21 18:53:19 +0300

    Reset Password Via Email Use Flask-mail and itsdangerous packages. Add reset password request templates, forms and routes. Generate reset tokens with itsdangerous pkg that expires. Use Flask-mail to configure a gmail server. Generate an APP PASSW0RD from Google in order to be able to authenticate third-party apps (myaccount.google.com/apppasswords). Send a message on email with a link to reset_token template. Save the user's new password on DB. Add an .env file for credentials.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,16 +1,29 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '9b59eb1ea57fd9f6d78e2c0559229b99'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app) # Set a DB instance
 bcrypt = Bcrypt(app) # Set an encrypting instance
 login_manager = LoginManager(app) # Set a Login manager instance
 login_manager.login_view = 'login' # Set the login manager by the login view
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
+mail = Mail(app)
+
 
 from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
 
@@ -14,6 +15,20 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def get_reset_token(self, expires_in=600):
+        s = Serializer(app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id})
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
 
     def __repr__(self):
         return f'User({self.username},{self.email}, {self.image_file})'
