e1d44bca46c0c6a3ad98b70c5c82d6c693cdf64f
Liliana Candrea <candrealiliana94@gmail.com>
2026-04-22 13:30:36 +0300

    Add configuration class based.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,28 +1,22 @@
-import os
 from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-
+from flaskblog.config import Config
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
+
 db = SQLAlchemy(app) # Set a DB instance
 bcrypt = Bcrypt(app) # Set an encrypting instance
 login_manager = LoginManager(app) # Set a Login manager instance
-login_manager.login_view = 'login' # Set the login manager by the login view
+login_manager.login_view = 'users.login' # Set the login manager by the login view
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
+
 mail = Mail(app)
 
 from flaskblog.main.routes import main
